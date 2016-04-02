01c0c8419493c9c4011571182e7aa8a6921c5a39
sahileo <sahil.jayakar@gmail.com>
2016-04-03 03:41:41 +0530

    Coursera_Exploratory Data Analysis_Project1_Part1

---
+++ plot1.R
@@ -0,0 +1,24 @@
+##COURSERA: EXPLORATORY DATA ANALYSIS
+#PROJECT 1:
+#Assignment 1:
+
+setwd("C:\\Users\\Sahil\\Desktop\\Coursera_EDA")
+
+#Import data
+  data <- read.csv("household_power_consumption.txt",header=TRUE,sep=";", stringsAsFactors = FALSE)
+  head(data,2) #Check loaded data
+  nrow(data) #2075259
+  str(data)
+#Subset data based on date
+  data <- subset(data, data$Date %in% c("1/2/2007","2/2/2007"))
+  nrow(data) #2880
+#Change class of date variable to 'date'
+  data$Date <- as.Date(data$Date)
+  class(data$Date) #Date
+#Change class of 'Global_active_power' variable to numeric for plotting
+  data$Global_active_power <- as.numeric(data$Global_active_power)
+  class(data$Global_active_power) #Numeric
+#Plot and save as png file
+  hist(data$Global_active_power, col="red", main = "Global Active Power", ylab="Frequncy",xlab="Global Active Power (kilowatts)")
+  dev.copy(png, "plot1.png", width=480, height=480)
+  dev.off()
